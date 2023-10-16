6950253a50ae6adf6faa706208bf363b42a7079e
whauzan <wahyu172845@gmail.com>
2023-10-17 03:34:22 +0700

    feat: add auth with clerk

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="primary-text-gradient h1-bold">CodeSage</h1>
+      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }
