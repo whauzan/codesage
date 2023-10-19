86a1f32025bdab6509bc1c067a9816a180e64f5d
whauzan <wahyu172845@gmail.com>
2023-10-20 05:19:55 +0700

    feat: home page

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,9 +1,51 @@
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/home/HomeFilter";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
+
+const questions = [
+  {
+    _id: "1",
+    title: "Redux Toolkit Not Updating State as Expected",
+    tags: [
+      { _id: "2", name: "react" },
+      { _id: "5", name: "redux" },
+    ],
+    author: {
+      _id: "author1",
+      name: "John Doe",
+      picture: "https://github.com/shadcn.png",
+      clerkId: "clerk1",
+    },
+    upvotes: 1000000,
+    views: 100,
+    answers: [
+      { answerId: "answer1", text: "This is the answer." },
+      { answerId: "answer2", text: "Another answer here." },
+    ],
+    createdAt: new Date(),
+    clerkId: "clerk1",
+  },
+  {
+    _id: "2",
+    title: "How to center a div",
+    tags: [{ _id: "2", name: "react" }],
+    author: {
+      _id: "author2",
+      name: "Jane Smith",
+      picture: "https://github.com/shadcn.png",
+      clerkId: "clerk2",
+    },
+    upvotes: 5,
+    views: 50,
+    answers: [],
+    createdAt: new Date(),
+  },
+];
 
 export default function Home() {
   return (
@@ -11,7 +53,7 @@ export default function Home() {
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
-        <Link href="/ask-questions" className="flex justify-end max-sm:w-full">
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
           <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
             Ask a Questions
           </Button>
@@ -34,6 +76,33 @@ export default function Home() {
       </div>
 
       <HomeFilter />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+          discussion. Our query could be the next big thing others learn from. Get
+          involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
---
+++ src/components/cards/QuestionCard.tsx
@@ -0,0 +1,96 @@
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
+import RenderTag from "../shared/RenderTag";
+import Metric from "../shared/Metric";
+
+interface Props {
+  _id: string;
+  title: string;
+  tags: {
+    _id: string;
+    name: string;
+  }[];
+  author: {
+    _id: string;
+    name: string;
+    picture: string;
+    clerkId: string;
+  };
+  upvotes: number;
+  views: number;
+  answers: Array<object>;
+  createdAt: Date;
+  clerkId?: string | null;
+}
+
+const QuestionCard = ({
+  _id,
+  title,
+  tags,
+  author,
+  upvotes,
+  views,
+  answers,
+  createdAt,
+}: Props) => {
+  return (
+    <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
+      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+        <div>
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+            {getTimestamp(createdAt)}
+          </span>
+          <Link href={`/quetion/${_id}`}>
+            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+              {title}
+            </h3>
+          </Link>
+        </div>
+      </div>
+
+      <div className="mt-3.5 flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <RenderTag key={tag._id} name={tag.name} _id={tag._id} />
+        ))}
+      </div>
+
+      <div className="flex-between mt-6 w-full flex-wrap gap-3">
+        <Metric
+          imgUrl={author.picture}
+          alt="User"
+          value={author.name}
+          title={` • asked ${getTimestamp(createdAt)}`}
+          href={`/profile/${author._id}`}
+          textStyles="body-medium text-dark400_light700"
+          isAuthor
+        />
+        <div className="flex items-center gap-3 max-sm:flex-wrap max-sm:justify-start">
+          <Metric
+            imgUrl="/assets/icons/like.svg"
+            alt="Upvotes"
+            value={formatAndDivideNumber(upvotes)}
+            title="Votes"
+            textStyles="small-medium text-dark400_light800"
+          />
+          <Metric
+            imgUrl="/assets/icons/message.svg"
+            alt="Message"
+            value={formatAndDivideNumber(answers.length)}
+            title="Answers"
+            textStyles="small-medium text-dark400_light800"
+          />
+          <Metric
+            imgUrl="/assets/icons/eye.svg"
+            alt="Eye"
+            value={formatAndDivideNumber(views)}
+            title="Views"
+            textStyles="small-medium text-dark400_light800"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
