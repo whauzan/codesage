85c50e0e9caf749b4a49f571c1c96278c0cc1bbb
whauzan <wahyu172845@gmail.com>
2023-10-21 00:41:26 +0700

    feat: prepare nextjs server action

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    serverActions: true,
+    mdxRs: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
 };
 
 module.exports = nextConfig;
