66ca58e9e389e96b1051e55a296d8e6fdf5b3c7f
Wahyu Hauzan Rafi <whauzan@air.local>
2023-12-10 02:08:49 +0700

    feat: tag detail page

--- src/lib/actions/shared.types.d.ts
+++ src/lib/actions/shared.types.d.ts
@@ -108,3 +108,10 @@ export interface ViewQuestionParams {
   questionId: string;
   userId: string | undefined;
 }
+
+export interface GetQuestionsByTagIdParams {
+  tagId: string;
+  page?: number;
+  pageSize?: number;
+  searchQuery?: string;
+}
