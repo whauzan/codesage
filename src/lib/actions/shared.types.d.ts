28324632986ee264b6fb65036234962318d8b094
Wahyu Hauzan Rafi <whauzan@air.local>
2023-12-17 03:06:21 +0700

    feat: profile page

--- src/lib/actions/shared.types.d.ts
+++ src/lib/actions/shared.types.d.ts
@@ -115,3 +115,9 @@ export interface GetQuestionsByTagIdParams {
   pageSize?: number;
   searchQuery?: string;
 }
+
+export interface GetUserStatsParams {
+  userId: string;
+  page?: number;
+  pageSize?: number;
+}
