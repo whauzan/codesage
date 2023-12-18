deb88aa95320253521627601b6cc22fe67853d1b
Wahyu Hauzan Rafi <whauzan@air.local>
2023-12-19 02:19:49 +0700

    feat: get Top questions and popular tags from db

--- src/components/shared/RightSidebar.tsx
+++ src/components/shared/RightSidebar.tsx
@@ -2,37 +2,22 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
+import { getTopQuestions } from "@/lib/actions/question.action";
+import { getPopularTags } from "@/lib/actions/tag.actions";
 
-const hotQuestions = [
-  { _id: "1", title: "How do I use express as a custom server in NextJS?" },
-  { _id: "2", title: "Redux Toolkit Not Updating State as Expected" },
-  { _id: "3", title: "Async/Await Function Not Handling Errors Properly" },
-  {
-    _id: "4",
-    title:
-      "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
-  },
-  { _id: "5", title: "How to Perfectly center a Div with Tailwind CSS?" },
-];
+const RightSidebar = async () => {
+  const topQuestions = await getTopQuestions();
+  const popularTags = await getPopularTags();
 
-const popularTags = [
-  { _id: "1", name: "javascript", totalQuestions: 5 },
-  { _id: "2", name: "react", totalQuestions: 15 },
-  { _id: "3", name: "next", totalQuestions: 10 },
-  { _id: "4", name: "vue", totalQuestions: 20 },
-  { _id: "5", name: "redux", totalQuestions: 10 },
-];
-
-const RightSidebar = () => {
   return (
     <section className="custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
         <div className="mt-7 flex w-full flex-col gap-[30px]">
-          {hotQuestions.map((question) => (
+          {topQuestions.map((question) => (
             <Link
               key={question._id}
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
               <p className="body-medium text-dark500_light700">
@@ -57,7 +42,7 @@ const RightSidebar = () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
