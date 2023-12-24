d9f852b3560bd499d67a47282cb94827e5fcb7f4
Wahyu Hauzan Rafi <whauzan@air.local>
2023-12-25 02:36:17 +0700

    fix: sorting most answered in collection page

--- src/database/question.model.ts
+++ src/database/question.model.ts
@@ -9,6 +9,7 @@ export interface IQuestion extends Document {
   downvotes: Schema.Types.ObjectId[];
   author: Schema.Types.ObjectId;
   answers: Schema.Types.ObjectId[];
+  answersCount: number;
   createdAt: Date;
 }
 
@@ -21,7 +22,14 @@ const QuestionSchema = new Schema({
   downvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
   author: { type: Schema.Types.ObjectId, ref: "User" },
   answers: [{ type: Schema.Types.ObjectId, ref: "Answer" }],
+  answersCount: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
+});
+
+QuestionSchema.post("findOneAndUpdate", async function () {
+  const docToUpdate = await this.model.findOne(this.getQuery());
+  docToUpdate.answersCount = docToUpdate.answers.length;
+  await docToUpdate.save();
 });
 
 const Question = models.Question || model("Question", QuestionSchema);
